3c7ba335c1e26b496e4a0ab796cb56dcca171fe9
T1 <prechaya.maks@kmutt.ac.th>
2024-10-14 16:45:23 +0700

    g05-feat/49/Review+DashboardPage

--- src/modules/online-exam/pages/professor/ProfessorOverallScoringPage.jsx
+++ src/modules/online-exam/pages/professor/ProfessorOverallScoringPage.jsx
@@ -7,41 +7,43 @@ export default function ProfessorOverallScoringPage() {
     <>
       {/* <NavBar /> */}
       {/* Heading */}
-      <div className="flex justify-between items-center px-[26px] py-[35px] lg:px-[200px]">
-        <div>
-          <p className="font-bold text-[#D4A015] text-[30px]">Scoring</p>
-          <p className="text-[16px]">Linear Algebra Exam</p>
+      <div className="px-[26px]  lg:px-[200px]">
+        <div className="flex justify-between items-center py-[35px]">
+          <div>
+            <p className="font-bold text-[#D4A015] text-[30px]">Scoring</p>
+            <p className="text-[16px]">Linear Algebra Exam</p>
+          </div>
+          <div>
+            <PublishScoreButton />
+          </div>
         </div>
-        <div>
-          <PublishScoreButton />
-        </div>
-      </div>
-      <hr className="border-t border-[#798184]" />
-      {/* content */}
-      <div className="px-[26px] py-[30px] lg:px-[200px]">
-        <p className="font-bold text-[#D4A015] text-[22px] ">Participants</p>
-        <div className="flex pb-[10px]">
-          <p className="text-[16px] font-bold">
-            Scoring finished:<span>&nbsp;</span>
-          </p>
-          <p className="">40</p>
-          <p className="">/</p>
-          <p className="">100</p>
-          <p className="">
-            <span>&nbsp;</span>student
-          </p>
-        </div>
+        <hr className="border-t border-[#798184]" />
+        {/* content */}
+        <div className=" py-[30px] ">
+          <p className="font-bold text-[#D4A015] text-[22px] ">Participants</p>
+          <div className="flex pb-[10px]">
+            <p className="text-[16px] font-bold">
+              Scoring finished:<span>&nbsp;</span>
+            </p>
+            <p className="">40</p>
+            <p className="">/</p>
+            <p className="">100</p>
+            <p className="">
+              <span>&nbsp;</span>student
+            </p>
+          </div>
 
-        <p className="w-[100%] flex rounded-lg text-[12px] items-center lg:text-[14px] px-[20px] py-[15px] gap-[10px]">
-          <p className="w-[30%]">ID number</p>
-          <p className="w-[35%]">Name</p>
-          <p className="w-[10%]">Score</p>
-          <p className="w-[10%]">Total score</p>
-          <p className="w-[15%]">Status</p>
-        </p>
-        <hr className="border-t border-[#798184] pb-[20px]" />
+          <p className="w-[100%] flex rounded-lg text-[12px] items-center lg:text-[14px] px-[20px] py-[15px] gap-[10px]">
+            <p className="w-[30%]">ID number</p>
+            <p className="w-[35%]">Name</p>
+            <p className="w-[10%]">Score</p>
+            <p className="w-[10%]">Total score</p>
+            <p className="w-[15%]">Status</p>
+          </p>
+          <hr className="border-t border-[#798184] pb-[20px]" />
 
-        <Participant />
+          <Participant />
+        </div>
       </div>
     </>
   );
