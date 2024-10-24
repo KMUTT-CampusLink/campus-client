289c5c9bf573f4df8eaee2493fc6baed342fce74
shinemin19 <shinemin.khan@kmutt.ac.th>
2024-10-24 16:57:22 +0700

    final design I think

--- src/modules/chatbot/components/chatPage/FaqCard.jsx
+++ src/modules/chatbot/components/chatPage/FaqCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FaqCard = ({faq, startChat, textRef, setButtonAble}) => {
   return (
-    <div className={`cursor-pointer shadow-slate-600 border rounded-xl text-left ${startChat? `text-sm p-[6px] sm:p-[10px]`: `text-lg font-semibold p-3 sm:p-4 `}`}
+    <div className={`cursor-pointer shadow-slate-600 border rounded-xl text-left ${startChat? `text-sm p-[6px] sm:p-[10px]`: `text-md font-semibold p-3 sm:p-4 `}`}
       onClick={() => {textRef.current.value = faq; setButtonAble(true); }}>
       {faq}
     </div>
