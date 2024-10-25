c0475fdd3e200cd9ff1aa55ebd62045adf6cf184
Nudhana Sarutipaisan <nu37317@gmail.com>
2024-10-25 11:43:00 +0700

    g05-bugfix/43/handleAnswerIsNotFunction

---
+++ src/modules/online-exam/components/student/ReviewExamPage/Question.jsx
@@ -0,0 +1,56 @@
+import { faChessKing } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+
+export default function Question({ questionNo, question, choice, type, studentAnswer }) {
+//   const isSelected = (value) => {
+//     return localAnswer.includes(value) ? "checked" : "";
+//   };
+
+  console.log(choice);
+
+  return (
+    <div className="border border-[#BEBEBE] rounded-xl p-[25px] w-full">
+      <h1>{questionNo + 1}. {question}</h1>
+      <div className="flex flex-col gap-[10px] pt-[20px]">
+        {choice && choice.map((choiceObj, index) => (
+          <div key={index} className="flex items-center gap-[10px]">
+            {/* Multiple Choice */}
+            {type === 'Multiple Choice' && (
+              <>
+                <input
+                  type="radio"
+                  name={`radio-${questionNo}`}
+                  className={`radio checked:bg-[#C76650]`}
+                //   checked={isSelected(choiceObj.choiceText)}
+                  onChange={() => handleInputChange(choiceObj.choiceText)}
+                />
+                {choiceObj}
+              </>
+            )}
+            {/* Checklist */}
+            {type === 'Checklist' && (
+              <>
+                <input
+                  type="checkbox"
+                  className={`checkbox [--chkbg:#C76650] [--chkfg:white] checked:border-[#C76650]`}
+                //   checked={isSelected(choiceObj.choiceText)}
+                  onChange={() => handleInputChange(choiceObj.choiceText)}
+                />
+                {choiceObj}
+              </>
+            )}
+          </div>
+        ))}
+        {/* Essay */}
+        {type === 'Essay' && (
+          <textarea
+            className="textarea textarea-bordered border-[#BEBEBE] w-full h-[220px]"
+            placeholder="Type your Answer Here"
+            // value={localAnswer[0] || ''}
+            onChange={(e) => handleInputChange(e.target.value)}
+          ></textarea>
+        )}
+      </div>
+    </div>
+  );
+}
