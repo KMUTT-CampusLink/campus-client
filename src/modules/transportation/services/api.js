7a85781d6f677d680d511e6b68ea6b865c50063b
Hejdula <hejdula@proton.me>
2024-10-26 19:51:15 +0700

    added endpoint to fetch trips by route id with schedule

--- src/modules/transportation/services/api.js
+++ src/modules/transportation/services/api.js
@@ -34,8 +34,37 @@ export const fetchAllStops = () => get("/transport/user/queryAllStops");
 /**
  * Fetches the route connecting two stops.
  *
- * @param {string} startStopID - The ID of the starting stop.
- * @param {string} endStopId - The ID of the ending stop.
+ * @param {int} startStopID - The ID of the starting stop.
+ * @param {int} endStopId - The ID of the ending stop.
  */
 export const fetchRoutesConnectingStops = (startStopID, endStopId) =>
   get(`/transport/user/routesConnectingStops/${startStopID}/${endStopId}`);
+
+/**
+ * Fetches the trips for a route together with the routes schedule.
+ * Returns JSON: { trips: [{ id, driver_id, trip_schedule_id, vehicle_id, trip_date, trip_schedule:{ id, route_id, day, start_time, end_time, status}}]
+ * @param {int} routeID - The ID of the route
+ * @example
+ * example return value:
+ * {
+ *   "trips": [
+ *     {
+ *       "id": 1,
+ *       "driver_id": 1,
+ *       "trip_schedule_id": 1,
+ *       "vehicle_id": 1,
+ *       "trip_date": "2024-10-31T00:00:00.000Z",
+ *       "trip_schedule": {
+ *         "id": 1,
+ *         "route_id": 1,
+ *         "day": "Monday",
+ *         "start_time": "1970-01-01T02:00:00.000Z",
+ *         "end_time": "1970-01-01T03:00:00.000Z",
+ *         "status": true,
+ *       }
+ *     },
+ *   ]
+ * }
+ */
+export const fetchTripsByRouteID = (routeID) =>
+  get(`/transport/user/queryTripsByRouteID/${routeID}`);
