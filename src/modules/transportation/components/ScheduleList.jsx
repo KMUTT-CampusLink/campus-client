8c3a2c453dab65c3f687924feac34fb66af8f9c7
Hejdula <hejdula@proton.me>
2024-10-31 12:46:15 +0700

    adding info and polishing schedule, only functionality, not pretty yet

--- src/modules/transportation/components/ScheduleList.jsx
+++ src/modules/transportation/components/ScheduleList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Display a list of trips for a route, filtering of what trips are shown is yet to be done, ie those which already hapenned should not be shown
 const TripList = ({ trips }) => {
   return (
     <div className="w-full">
@@ -16,13 +17,15 @@ const TripList = ({ trips }) => {
             value={trip.id}
             className="flex flex-row bg-orange-100 shadow-lg rounded-lg p-6 w-full justify-between mb-4"
           >
+            {/* info about the trip, not pretty for now */}
             <div>
-              <h3 className="text-2xl font-bold mb-2">
-                {trip.trip_schedule.start_time}
-              </h3>
-              <h3 className="text-2xl font-bold mb-2">
-                {trip.trip_schedule.end_time}
-              </h3>
+              <h1>Driver (only id for now) : {trip.driver_id}</h1>
+              <h1>Vehicle (only id for now) : {trip.vehicle_id}</h1>
+              <h1>start time: {trip.trip_schedule.start_time}</h1>
+              <h1>start time: {trip.trip_schedule.start_time}</h1>
+              <h1>end time: {trip.trip_schedule.end_time}</h1>
+              <h1>date: {trip.trip_date}</h1>
+              <h1>day of week: {trip.trip_schedule.day}</h1>
               {/* Button, eventually link to booking confirmation */}
               <button className="mt-auto py-2 px-4 bg-orange-500 text-white rounded-lg hover:bg-orange-600 transition">
                 Book
