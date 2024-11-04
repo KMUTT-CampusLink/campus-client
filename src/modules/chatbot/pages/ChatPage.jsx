51161bf15bfa066a7a9fdd197e40f4ff82e05b48
shinemin19 <shinemin.khan@kmutt.ac.th>
2024-11-04 19:51:02 +0700

    design updated with recommend ques

--- src/modules/chatbot/pages/ChatPage.jsx
+++ src/modules/chatbot/pages/ChatPage.jsx
@@ -16,26 +16,35 @@ const ChatPage = () => {
   const [dummyAns, setDummyAns] = useState([]);
   const [startChat, setStartChat] = useState(false);
   const textRef = useRef(null);
-
+  const studentId = localStorage.getItem("studentId");
+  
   const botImage = "chatbot/botImage.jpg";
+
   useEffect(() => {
-    const fetchFaqs = async () => {
-        try{
-            const response = await axiosInstance.get('/botastra/faqs');
-            const questions = response.data.map(faq => faq.question);
-            setDummyFaqs(questions);
-            setLoading(false);
-        }
-        catch(error){
-            setError(error.message);
-            setLoading(false);
-        }
+    const fetchNextQuestions = async() => {
+      try{
+        const response = await axiosInstance.get('/botastra/faqs');
+        const questions = response.data.map(faq => faq.question);
+        setDummyFaqs(questions);
+        setLoading(false);
+      }
+      catch(error){
+          setError(error.message);
+          setLoading(false);
+      }
     }
+    fetchNextQuestions();
+  }, [])
+
+  useEffect(() => {
     const generateSessionId = () => {
       setSessionId(Math.random().toString(36).substring(7));
     }
-    generateSessionId();
-    fetchFaqs();
+    if(!studentId){
+      generateSessionId();
+    }else{
+      setSessionId(studentId);
+    }
   }, [])
 
   function autoResize(e) {
@@ -47,7 +56,6 @@ const ChatPage = () => {
   }
 
   const askQuestion = async (e) => {
-    // Prevent the default form submit behavior if 'e' is defined (it's a React event handler)
     if (e) e.preventDefault();
     const input = textRef.current.value.trim();
     textRef.current.value = "";
@@ -57,8 +65,15 @@ const ChatPage = () => {
       setQuestions([...questions, input]);
       try {
         const reply = await axiosInstance.post("/botastra/message", {message : input, sessionId: sessionId} );
-        console.log(reply.data.replyMessage);
-        const ans = reply.data.replyMessage;
+        const ans = reply.data.replyText;
+        const nextQuestions = reply.data.nextQuestions;
+        if(nextQuestions.length > 0){
+          let nextQs = [];
+          nextQuestions.map((que, index) => {
+            nextQs[index] = que.next_question;
+          })
+          setDummyFaqs(nextQs);
+        }
         setDummyAns([...dummyAns, ans]);
       } catch (error) {
         console.error("Error sending message:", error);
@@ -71,12 +86,12 @@ const ChatPage = () => {
   }
 
   return (
-    <div className = " min-w-screen min-h-screen">
+    <div className = " min-w-screen min-h-screen" style={{backgroundImage: "linear-gradient(to bottom, rgba(252, 219, 165, 1), rgba(248, 248, 248, 1)"}}>
         <NavBar />
         {/* main div for chat */}
-        <div className = {` mx-auto max-w-7xl pt-16 sm:pt-10 md:pt-6 pb-6 sm:pb-4 md:pb-0 w-full sm:w-[90%] h-screen rounded-t-xl flex flex-col items-center justify-between`}>
+        <div className = {` mx-auto max-w-7xl pt-16 sm:pt-10 md:pt-6 pb-6 sm:pb-4 md:pb-0 w-full sm:w-[90%] h-screen rounded-t-xl flex flex-col items-center justify-end`}>
             {/* messaging area  */}
-            <div className= { ` w-full sm:w-5/6 flex flex-col h-[80%] ${startChat? `justify-between py-0 sm:py-2 md:py-4`: `justify-around py-8 sm:py-12 md:py-16`} `}>
+            <div className= { ` w-full sm:w-5/6 flex ${startChat? `gap-0 pt-0 mt-14 pb-0 mb-0 flex-col-reverse h-[100%] `: ` h-[80%] flex-col py-8 sm:py-12`} justify-start`}>
                 {/* bot profile div  */}
                 <div className= {`${startChat ? `hidden`: `visible`} w-full h-fit flex justify-center items-center `}>
                     <img src={botImage} alt="batman" className="object-contain w-[6rem] sm:w-[10rem] h-auto rounded-full"/>
@@ -84,14 +99,11 @@ const ChatPage = () => {
                 {/* faqs div  */}
                 {loading ? <div>
                     <p className="text-center font-bold text-2xl sm:text-4xl md:text-5xl">Loading FAQs...</p>
-                </div> : <div className={` w-full ${startChat? `min-h-[10%] p-0`: `h-[60%] sm:h-[65%] md:h-[70%] p-10`} flex justify-center items-center`}>
-                    <div className={`gap-5 flex flex-col p-8 sm:p-10`}>
-                        <p className = {`${startChat ? `hidden` : `visible`} font-bold text-xl`}>Try asking these questions!</p>
+                </div> : <div className={` w-full ${startChat? `min-h-[8%] p-0`: `h-[60%] sm:h-[65%] pb-0`} flex justify-center items-start`}>
+                    <div className={`${startChat ? `p-2 px-6 gap-3` :`gap-5 flex flex-col p-8 sm:p-10` }`}>
                         <FAQs textRef={textRef} dummyFaqs = {dummyFaqs} startChat = {startChat} setButtonAble = {setButtonAble}/>
                     </div>
                 </div>}
-                
-
                 {/* messaging area */}
                 {startChat ? 
                     <MessageArea questions = {questions} dummyAns = {dummyAns} profilePic = {botImage}/> : <></>
@@ -99,13 +111,13 @@ const ChatPage = () => {
             </div>
             {/* chat input area  */}
             <div className=' w-full h-fit flex flex-col justify-center items-center sm:pb-6 md:pb-8'>
-                <form action="#" className={`bg-[#D9D9D9] shadow-lg w-4/5 min-h-[1rem] max-h-fit rounded-[1.75rem] flex flex-row justify-between px-4 py-1 gap-4 `}>
+                <form action="#" className={`bg-[white] shadow-lg w-4/5 min-h-[1rem] max-h-fit rounded-[1.75rem] flex flex-row justify-between px-4 py-1 gap-4 `}>
                     <div className='flex w-full h-fit'>
                         <textarea
                         placeholder="How may I help you..."
                         className="w-full px-4 py-3 rounded-xl min-h-[1rem] sm:min-h-[1.5rem] max-h-[3.75rem] sm:max-h-[4rem] border-none outline-none font-semibold text-black
                         text-md sm:text-lg placeholder-black placeholder-opacity-65
-                        resize-none overflow-hidden bg-[#D9D9D9]"
+                        resize-none overflow-hidden bg-[white]"
                         onChange={autoResize}
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' && !e.ctrlKey && e.target.value) {
