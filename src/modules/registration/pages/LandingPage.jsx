ac2aebbae4ea0bf33d62c85eebacd81f66efc52f
Hein <nwaimarn@gmail.com>
2024-11-08 00:13:57 +0700

    footer card draft

--- src/modules/registration/pages/LandingPage.jsx
+++ src/modules/registration/pages/LandingPage.jsx
@@ -1,3 +1,4 @@
+import FooterCard from "../components/FooterCard";
 import NavBar from "../components/NavBarComponents/NavBar";
 import { mainStyles, containerDivStyles, logoHead } from "../styles/styles";
 
@@ -5,18 +6,8 @@ function LandingPage() {
   return (
     <div className={containerDivStyles}>
       <NavBar />
-      <main className={mainStyles}>
-        <h2 className={`${logoHead} p-10`}>CampusLink</h2>
-
-        <div className="rounded-xl mx-auto text-black">
-          <div
-            className="flex items-center justify-center mx-auto sm:w-96 h-60 rounded-2xl"
-            style={{
-              backgroundImage: "linear-gradient(to right, #c2544d, #f09107)",
-            }}
-          ></div>
-        </div>
-      </main>
+      <main className={mainStyles}></main>
+      <FooterCard />
     </div>
   );
 }
--- src/modules/registration/styles/styles.js
+++ src/modules/registration/styles/styles.js
@@ -1,6 +1,6 @@
 export const mainStyles = "mx-auto max-w-7xl pt-20 pb-6 w-4/5";
 export const containerDivStyles =
-  "min-h-screen bg-gradient-to-b from-[rgba(194,84,77,0.5)] via-[rgba(238,143,9,0.5)] to-[rgba(255,234,191,0.5)]";
+  "min-h-screen bg-gray-200";
 export const formHead = "text-center text-2xl font-bold text-[#864e41] mb-4";
 export const formBg = "bg-[#F6F6F6] rounded-lg shadow-md p-6 mb-10";
 export const logoHead =
