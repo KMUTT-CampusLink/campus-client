f42e6d1821224813fdea90ea954a0af42fdc76c3
MR. PUNYAPAT JAISUK <66130500850@st.sit.kmutt.ac.th>
2024-11-21 02:19:43 +0700

    add auth

--- src/modules/attendance/routes/AttendRoutes.jsx
+++ src/modules/attendance/routes/AttendRoutes.jsx
@@ -3,28 +3,30 @@ import QrPage from "../page/QrPage";
 import StAttendancePage from "../page/StAttendancePage";
 import StQrPage from "../page/StQrPage";
 import FaceAttendancePage from "../page/faceAttendacePage";
-import QrScannerComponent from "../components/ScannerComponent";
+import AuthRoute from "../../registration/middleware/AuthRoute";
 export default function AttendRoutes() {
   return [
+    
     {
       path: "",
-      element: <AttendancePage />,
+      element: (<AuthRoute allowed_roles={["Professor"]}><AttendancePage /></AuthRoute>),
     },
+    
     {
       path: "qr",
-      element: <QrPage />,
+      element: (<AuthRoute allowed_roles={["Professor"]}><QrPage /></AuthRoute>),
     },
     {
       path: "stQr",
-      element: <StQrPage />,
+      element: (<AuthRoute allowed_roles={["Student"]}><StQrPage /></AuthRoute>),
     },
     {
       path: "statt",
-      element: <StAttendancePage/>,
+      element: (<AuthRoute allowed_roles={["Student"]}><StAttendancePage /></AuthRoute>),
     },
     {
       path: "faceAttendance",
-      element: <FaceAttendancePage/>
+      element: (<AuthRoute allowed_roles={["Professor"]}><FaceAttendancePage /></AuthRoute>)
     }
 
   ];
