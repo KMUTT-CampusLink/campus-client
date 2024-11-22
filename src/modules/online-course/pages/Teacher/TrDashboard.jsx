80ad718f593e2ff616a33232b0cf2310a703def4
Hnin Ei Ei Aung <hnineieiaung112@gmail.com>
2024-11-22 14:11:40 +0700

    course card animation and course card ui changes

--- src/modules/online-course/pages/Teacher/TrDashboard.jsx
+++ src/modules/online-course/pages/Teacher/TrDashboard.jsx
@@ -34,23 +34,23 @@ const TrDashboard = () => {
           searchFunction={searchFunction}
         />
         <div className="w-3/4 m-auto">
-          <span className="text-4xl font-bold pb-3 2xl:text-6xl">
+          <span className="text-3xl font-bold pb-4 2xl:text-5xl">
             All Courses
           </span>
           <div
-            className="grid max-md:grid-cols-2 grid-cols-3 p-5 gap-10 max-md:p-5 max-md:gap-6 max-sm:p-2
-            justify-items-center mx-auto 2xl:max-w-[90%] max-w-7xl"
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-2 sm:gap-8 lg:gap-10 justify-items-center mx-auto max-w-7xl 2xl:max-w-[90%]"
           >
             {filteredCourses && filteredCourses.length > 0 ? (
               filteredCourses.map((course) => (
                 <CourseCard key={course.sec_id} course={course} />
               ))
             ) : (
-              <p className="col-span-3 text-center text-gray-500">
+              <p className="col-span-1 sm:col-span-2 lg:col-span-3 text-center text-gray-500">
                 No courses found
               </p>
             )}
           </div>
+
         </div>
       </div>
     </div>
