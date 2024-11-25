88ef7638b51fa9ae6b4ce85b2af2ddef6e667bb1
LilEurey <134917527+LilEurey@users.noreply.github.com>
2024-11-25 17:24:14 +0700

    Add Guard Reservation

---
+++ src/modules/building-security/pages/administrator/AdministratorGuardReservation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "../../../registration/components/NavBarComponents/NavBar";
+import { useNavigate } from "react-router-dom";
+
+export default function AdministratorGuardReservation() {
+    
+}
